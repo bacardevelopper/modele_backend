b6c25f3adc214493ba4609fcbcf8d2b5e32e12fd
bacardevelopper <niwrad.pro@gmail.com>
2022-03-31 20:56:01 +0100

    dans b_logic

--- src/backend_arch/b_logic/crud_logic.js
+++ src/backend_arch/b_logic/crud_logic.js
@@ -1,3 +1,5 @@
+const { print } = require("../fct_utiles_et_test/console_log");
+
 exports.fct_read = (req, res, next) => {};
 exports.fct_update = (req, res, next) => {};
 exports.fct_delete = (req, res, next) => {};
--- src/backend_arch/b_logic/logic_log.js
+++ src/backend_arch/b_logic/logic_log.js
@@ -1,2 +1,10 @@
-exports.login_userControleur = (req, res, next) => {};
-exports.create_userCotroleur = (req, res, next) => {}
+const { print } = require("../fct_utiles_et_test/console_log");
+
+exports.login_userControleur = (req, res, next) => {
+  print("login path test");
+  res.status(200).json({ 'message' : "login path test"});
+};
+exports.create_userCotroleur = (req, res, next) => {
+  print("singup path test");
+  res.status(200).json({ 'message' : "signup path test"});
+};
